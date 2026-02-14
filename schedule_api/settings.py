d29e3407c8181b192b0364a1db0b1be297786c79
yanabelinskaya <144311460+yanabelinskaya@users.noreply.github.com>
2026-02-14 13:07:04 +0300

    Готовая

--- schedule_api/settings.py
+++ schedule_api/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
